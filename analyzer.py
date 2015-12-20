2bf4571509d22fb05d03b244b8813ac07b741f9f
Danny Behnecke <d.behnecke@tu-bs.de>
2015-12-20 15:56:20 +0100

    structure works, but not the right way

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,8 @@
+
+
 class TextAnalysis:
-
-    seperators = ['.', ',', ';', ':', '(', ')', ' ', '-', '[', ']', '{', '}', '?', '!', '&', '/']
-
+   # global separators
+    separators = ['.', ',', ';', ':', '(', ')', ' ', '-', '[', ']', '{', '}', '?', '!', '&', '/']
 
     def __init__(self):
         self.current_word = ''
@@ -9,14 +10,40 @@ class TextAnalysis:
         self.words =[]
         self.letters=[]
 
+    def knownObject(self,list, lw):
+        for i,sublist in enumerate(list):
+            for j,w in enumerate(sublist):
+                if lw == w:
+                    return (i,j)
+                else:
+                    return None
+
+
 
     def read(self,text):
         for w in text:
-            if not self.separators.contains(w):
+            if not w in TextAnalysis.separators:
                 self.current_word += w
-                self.letters.append(w) #TODO if letter is read twice, increment counter
-
-
+                availableLetter = self.knownObject(self.letters,w)
+                if availableLetter is not None:
+                    print availableLetter
+                    print availableLetter
+                    self.letters[availableLetter[0]][availableLetter[1]] += 1
+                else:
+                    self.letters.append([w, 1])
+                self.letters.append(w)
             else:
-                self.words.append(self.current_word)#TODO: if word is read twice, increment counter
+                availableWord = self.knownObject(self.words,self.current_word)
+                if availableWord is not None:
+                    self.words[availableWord[0]][availableWord[1]] += 1
+                else:
+                    self.words.append([self.current_word, 1])
                 self.current_word = ''
+
+if __name__ == '__main__':
+    testText = "Dies ist ein Testtext. Zwei Saetze als Test. Dies Dies Dies"
+    TA = TextAnalysis()
+
+    TA.read(testText)
+    print TA.letters
+    print TA.words
